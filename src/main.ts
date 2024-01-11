ec167e2fb24828f7c2c1c911ae946f10f84d0caf
GrilLove <2652511934@qq.com>
2024-01-11 21:16:51 +0800

    refactor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import './css/tailwindcss.css'
 import '@/assets/iconfont/iconfont.js'
 import '@/assets/iconfont/iconfont.css'
+import '@/style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,12 +2,29 @@
 <template>
   <HeaderVue></HeaderVue>
   <main>
-    <div class="bgMovie">
-      <video
-        width="100%"
-        src="../../../public/videos/MonaWallpaperFHD.mp4"
-      ></video>
-    </div>
+    <section>
+      <div class="bgMovie">
+        <video
+          width="100%"
+          src="../../../public/videos/MonaWallpaperFHD.mp4"
+          muted
+          autoplay
+          loop
+        ></video>
+      </div>
+    </section>
+    <section class="content max-w-screen-xl my-8 mx-auto flex justify-between">
+      <aside class="w-3/12 mr-4 overflow-hidden">
+        <div class="mb-4 h-96 bg-slate-400">这是侧边栏板块1</div>
+        <div class="mb-4 pb-4 h-48 bg-slate-400">这是侧边栏板块2</div>
+        <div class="mb-4 h-96 bg-slate-400">这是侧边栏板块3</div>
+      </aside>
+      <div class="w-9/12">
+        <div class="w-9/12 mb-4 h-80 bg-slate-500">这是内容板块1</div>
+        <div class="w-9/12 mb-4 h-80 bg-slate-500">这是内容板块2</div>
+        <div class="w-9/12 mb-4 h-80 bg-slate-500">这是内容板块3</div>
+      </div>
+    </section>
   </main>
 </template>
 
