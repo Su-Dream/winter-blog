abab9c32a12bb2db71f8b585fb4436057a769593
GrilLove <2652511934@qq.com>
2024-01-15 22:18:15 +0800

    feat:addcard-acc

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -15,6 +15,7 @@
     </section>
     <section class="content max-w-screen-xl my-8 mx-auto flex justify-between">
       <aside class="w-3/12 mr-4">
+        <!-- 用户信息板块 -->
         <div
           class="card-user-info mb-4 shadow-sm transform ease-in-out duration-300 hover:shadow-md px-6 py-5"
         >
@@ -96,10 +97,20 @@
             </div>
           </div>
         </div>
+        <!-- 公告板块 -->
         <div
-          class="card-acc pb-4 h-48 bg-slate-400 mb-4 shadow-sm transform ease-in-out duration-300 hover:shadow-md px-6 py-5"
+          class="card-acc pb-4 mb-4 shadow-sm transform ease-in-out duration-300 hover:shadow-md px-6 py-5"
         >
-          这是侧边栏板块2
+          <div class="acc mb-2">
+            <i class="iconfont icon-gonggao !text-xl align-middle"></i>
+            <span class="ml-[.625rem] align-middle font-bold">公告</span>
+          </div>
+          <div class="acc-info-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            doloremque deleniti possimus laborum ex nostrum et beatae cupiditate
+            fuga, nobis dignissimos, numquam labore minima omnis eius totam
+            similique molestias repellendus.
+          </div>
         </div>
         <div
           class="h-96 bg-slate-400 mb-4 shadow-sm transform ease-in-out duration-300 hover:shadow-md px-6 py-5"
