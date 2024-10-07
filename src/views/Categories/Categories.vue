00846b3ca73ab8c0adfe2894b5c5dfe31b66d63d
少女の梦 <2652511934@qq.com>
2024-10-07 12:39:37 +0800

    fix:修复categories样式问题

--- src/views/Categories/Categories.vue
+++ src/views/Categories/Categories.vue
@@ -1,11 +1,14 @@
 <template>
   <PageHeader></PageHeader>
-  <section class="content max-w-screen-xl my-8 mx-auto flex justify-between">
+  <!-- <section class="content max-w-screen-xl my-8 mx-auto flex justify-between"> -->
+  <section
+    class="content 2xl:w-7/12 xl:w-9/12 my-8 mx-auto flex justify-center flex-col-reverse items-center xl:items-start xl:flex-row xl:max-w-screen-2xl"
+  >
     <!-- 侧边栏 -->
     <CardAside></CardAside>
     <!-- 中间正文 -->
     <div
-      class="px-[2.5rem] py-[3.125rem] card-content-list w-9/12 text-regal-textColor shadow-sm transform ease-out duration-500 hover:shadow-md rounded-lg"
+      class="px-[2.5rem] py-[3.125rem] card-content-list w-11/12 text-regal-textColor shadow-sm transform ease-out duration-500 hover:shadow-md rounded-lg"
     ></div>
   </section>
 </template>
